6f65834d18546858333ebbadad4c1df8d2c03f98
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-11 14:39:58 +0800

    Update listing1-4.py

--- listing1-4.py
+++ listing1-4.py
@@ -8,3 +8,10 @@ def draw():
     screen.blit(images.mars, (50, 50))
     screen.blit(images.astronaut, (player_x,player_y))
     screen.blit(images.ship, (550, 300))
+
+def game_loop():
+    global player_x, player_y
+    if keyboard.right:
+        player_x += 5
+    elif keyboard.left:
+        player_x -= 5
