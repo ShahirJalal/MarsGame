945ee5308af780dd06d1a91cd72dfbf9636aeaa8
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-12 23:43:42 +0800

    Update listing3-5.py

--- listing3-5.py
+++ listing3-5.py
@@ -7,3 +7,12 @@ room_map = [ [1, 1, 1, 1, 1],
              [1, 1, 1, 1, 1]
              ]
 
+WIDTH = 800 # window size
+HEIGHT = 800
+top_left_x = 100
+top_left_y = 150
+
+DEMO_OBJECTS = [images.floor, images.pillar]
+
+room_height = 7
+room_width = 5
