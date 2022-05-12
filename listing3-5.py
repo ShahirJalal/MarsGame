aada122739c44c9a52b59dac0d01aac2deba0ce2
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-12 23:45:58 +0800

    Update listing3-5.py

--- listing3-5.py
+++ listing3-5.py
@@ -16,3 +16,8 @@ DEMO_OBJECTS = [images.floor, images.pillar]
 
 room_height = 7
 room_width = 5
+def draw():
+    for y in range(room_height):
+        for x in range(room_width):
+            image_to_draw = DEMO_OBJECTS[room_map[y][x]]
+            screen.blit(image_to_draw,(top_left_x + (x*30),top_left_y + (y*30) - image_to_draw.get_height()))
