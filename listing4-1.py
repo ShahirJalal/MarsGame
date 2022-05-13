35372388da6b6e6462f7c6d5619b96f3fff762bb
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-13 13:59:24 +0800

    Update listing4-1.py

--- listing4-1.py
+++ listing4-1.py
@@ -12,3 +12,9 @@ PLAYER_NAME = "Shahir"
 FRIEND1_NAME = "Aisyah"
 FRIEND2_NAME = "Azrai"
 current_room = 31 # start room = 31
+
+top_left_x = 100
+top_left_y = 150
+
+DEMO_OBJECTS = [images.floor, images.pillar, images.soil]
+
