4ac410bd18bb5d7e5554e1ad574a7d6ad76dbc82
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-13 14:00:15 +0800

    Update listing4-1.py

--- listing4-1.py
+++ listing4-1.py
@@ -25,3 +25,6 @@ DEMO_OBJECTS = [images.floor, images.pillar, images.soil]
 MAP_WIDTH = 5
 MAP_HEIGHT = 10
 MAP_SIZE = MAP_WIDTH * MAP_HEIGHT
+
+GAME_MAP = [ ["Room 0 - where unused objects are kept", 0, 0, False, False] ]
+
