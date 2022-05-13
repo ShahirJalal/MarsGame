cb28cc0e61683a4edee38e66799c9a2e655af4bd
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-13 13:26:33 +0800

    Create listing4-1.py

---
+++ listing4-1.py
@@ -0,0 +1,14 @@
+import time, random, math
+
+###############
+## VARIABLES ##
+###############
+
+WIDTH = 800 # window size
+HEIGHT = 800
+
+#PLAYER variables
+PLAYER_NAME = "Shahir"
+FRIEND1_NAME = "Aisyah"
+FRIEND2_NAME = "Azrai"
+current_room = 31 # start room = 31
