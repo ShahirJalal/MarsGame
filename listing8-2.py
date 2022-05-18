3de8f39ddcc56d7e8460de499386d717e934e437
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-05-18 15:12:44 +0800

    Update listing8-2.py

--- listing8-2.py
+++ listing8-2.py
@@ -83,6 +83,16 @@ PILLARS = [
     images.pillar_60, images.pillar_50
     ]
 
+wall_transparency_frame = 0
+
+BLACK = (0,0,0)
+BLUE = (0, 155, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255,255,255)
+GREEN = (0, 255, 0)
+RED = (128, 0, 0)
+
+
 #############
 ##   MAP   ##
 #############
